9b88d884335746b27460df0d8ce70214d3b26eee
Ange A. Buhendwa <mickaelbhd@gmail.com>
2026-03-10 11:35:32 +0200

    chore: fix merge conflict from develop

--- backend/src/main/java/com/servicehub/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/servicehub/exception/GlobalExceptionHandler.java
@@ -1,11 +1,11 @@
 package com.servicehub.exception;
 
 import com.servicehub.dto.ServerResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 import java.util.Map;

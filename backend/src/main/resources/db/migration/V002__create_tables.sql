ad3944ebacb103edf00d7bd82cc17aa0f60622ed
FiifiAkyerYawson <fiifi.yawson@amalitech.com>
2026-03-10 08:58:03 +0000

    set ddl-auto to validate, removed unnecessary pom.xml comment and set reasonable limits for varchar schema table colums

--- backend/src/main/resources/db/migration/V002__create_tables.sql
+++ backend/src/main/resources/db/migration/V002__create_tables.sql
@@ -15,8 +15,8 @@ CREATE TABLE IF NOT EXISTS departments (
 -- USERS
 CREATE TABLE IF NOT EXISTS users (
                                      id            SERIAL        PRIMARY KEY,
-                                     email         VARCHAR(255)  NOT NULL UNIQUE,
-    name          VARCHAR(150)  NOT NULL,
+                                     email         VARCHAR(30)  NOT NULL UNIQUE,
+    name          VARCHAR(50)  NOT NULL,
     password      VARCHAR(255)  NOT NULL,          -- bcrypt hash
     role          user_role     NOT NULL DEFAULT 'USER',
     department_id INT           REFERENCES departments (id) ON DELETE SET NULL,

82381e13f0c2bfe80c1629232716c25ea5243c03
Ange A. Buhendwa <mickaelbhd@gmail.com>
2026-03-11 17:56:11 +0200

    fix: update asyn configuration executor

--- backend/src/main/java/com/servicehub/config/AsyncConfig.java
+++ backend/src/main/java/com/servicehub/config/AsyncConfig.java
@@ -2,15 +2,16 @@ package com.servicehub.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.Executor;
 
 @Configuration
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
 
+  @Override
   @Bean
-  public ThreadPoolTaskExecutor asyncExecutor() {
+  public ThreadPoolTaskExecutor getAsyncExecutor() {
     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
     executor.setCorePoolSize(5);
     executor.setMaxPoolSize(25);

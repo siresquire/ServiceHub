d64676fb8eb2bfdc91c92a1f88702311536f0e5f
Shema Alphonse <ashema330@gmail.com>
2026-03-11 12:59:07 +0200

    chore :Update DepartmentRepository with findByName() and findByIsActiveTrue()

--- backend/src/main/java/com/servicehub/repository/DepartmentRepository.java
+++ backend/src/main/java/com/servicehub/repository/DepartmentRepository.java
@@ -3,8 +3,13 @@ package com.servicehub.repository;
 import com.servicehub.model.Department;
 import com.servicehub.model.enums.RequestCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
-    Optional<Department> findByCategory(RequestCategory category);
+    Optional<Department> findByName(String name);
+    List<Department> findByIsActiveTrue();
+    List<Department> findByCategory(RequestCategory category);
+    boolean existsByName(String name);
 }
